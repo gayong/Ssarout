61ad398441b5c537bd6f173e0a0636c8a0056418
swcl <swcl309@naver.com>
2023-07-25 16:07:18 +0900

    search input commponent

--- front-end/src/components/search/SearchBar.js
+++ front-end/src/components/search/SearchBar.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import {React,useState} from 'react';
+import { Link } from 'react-router-dom';
+
 
 const SearchBar = () => {
   // SearchBar 컴포넌트의 내용
+  const [keyword, setKeyword] = useState("")
+  const onChange =(event) => {
+    setKeyword(event.target.value)
+  }
   return (
-    <div>SearchBar 컴포넌트</div>
+    <div>
+      <input value={keyword} onChange={onChange} type='text' />
+      <button><Link to="/search" state={{data: keyword}}>search</Link></button>
+    </div>
   );
 };
 
