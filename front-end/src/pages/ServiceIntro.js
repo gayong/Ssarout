160f56c86a8f14fd4d44bb935851daedb0184310
공정민 <jmgong59@gmail.com>
2023-07-27 09:33:18 +0900

    [CHORE] : test

--- front-end/src/pages/ServiceIntro.js
+++ front-end/src/pages/ServiceIntro.js
@@ -1,12 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 
-const ServiceIntro = () => {
-  
-    return (
-      <div>
-        <h3>서비스 소개</h3>
-      </div>
-    );
+const IntroPage = ({ setShowIntro }) => {
+  const handleHideIntro = () => {
+    setShowIntro(false);
   };
-  
-  export default ServiceIntro
+
+  return (
+    <div>
+      {/* 서비스 소개 페이지 컨텐츠 */}
+      <p>서비스를 소개하는 내용입니다.</p>
+      <button onClick={handleHideIntro}>다시 보지 않기</button>
+    </div>
+  );
+};
+
+const MainPage = () => {
+  return (
+    <div>
+      {/* 메인 페이지 컨텐츠 */}
+      <p>메인 페이지 내용입니다.</p>
+    </div>
+  );
+};
+
+const App = () => {
+  const [showIntro, setShowIntro] = useState(true);
+
+  return (
+    <Router>
+      <Switch>
+        <Route path="/intro">
+          {showIntro ? <IntroPage setShowIntro={setShowIntro} /> : <Redirect to="/" />}
+        </Route>
+        <Route path="/" component={MainPage} />
+      </Switch>
+    </Router>
+  );
+};
+
+export default App;
