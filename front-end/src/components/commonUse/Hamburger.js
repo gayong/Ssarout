8b4aa03615768b2f96515958555f704a73366385
이가영 <lgy4625@naver.com>
2023-07-28 16:52:22 +0900

    "#S09P12E203-200 [FIX] : hamburger bar complete"

--- front-end/src/components/commonUse/Hamburger.js
+++ front-end/src/components/commonUse/Hamburger.js
@@ -1,57 +1,25 @@
-import React, { Component } from 'react';
-import { slide as Menu } from 'react-burger-menu';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
 import styles from "./Hamburger.module.css";
-import MenuIcon from '@mui/icons-material/Menu';
+import Sidebar from "./Sidebar";
 
-const BurgerMenuIcon = () => {
-  return <MenuIcon className={styles.burgerIcon} />;
-};
+function Burger() {
+  const [isOpen, setIsOpen] = useState(false);
 
-class MenuBar extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isOpen: false, // 사이드바의 열림/닫힘 상태 저장
-    };
-  }
-
-  handleBurgerIconClick = () => {
-    this.setState((prevState) => ({
-      isOpen: !prevState.isOpen,
-    }));
+  const toggleSide = () => {
+    setIsOpen(!isOpen);
   };
 
-  render() {
-    const { isOpen } = this.state;
-
-    return (
-      <div>
-        <BurgerMenuIcon onClick={this.handleBurgerIconClick} />
-        <Menu
-          right
-          width={'90%'}
-          isOpen={isOpen}
-          onStateChange={(state) => this.setState({ isOpen: state.isOpen })}
-          styles={{
-            bmMenuWrap: {
-              background: isOpen ? '#fff' : 'transparent', 
-            },
-          }}
-        >
-          <p className={styles.menuitem}>
-            로그인
-          </p>
-          <p className={styles.menuitem}>
-            회원가입
-          </p>
-          <p className={styles.mypagemenu}><Link to="/mypage">
-            마이페이지</Link>
-          </p>
-        </Menu>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <img
+        className={styles.burgerIcon}
+        alt="burger"
+        src="./burger.png"
+        onClick={toggleSide}
+      />
+      <Sidebar isOpen={isOpen} setIsOpen={setIsOpen} />
+    </div>
+  );
 }
 
-export default MenuBar;
+export default Burger;
---
+++ front-end/src/components/commonUse/Sidebar.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import styled from 'styled-components';
+import styles from './Sidebar.module.css'
+import { Link } from 'react-router-dom'
+
+
+const SideBarWrap = styled.div`
+  z-index: 5;
+  padding: 12px;
+  background-color: #fff;
+  height: 100%;
+  width: 55%;
+  right: -55%;
+  top: 0;
+  position: fixed;
+  transition: 0.5s ease;
+
+  display: flex;
+  flex-direction: column;
+
+  &.open {
+    right: 0;
+    transition: 0.5s ease;
+  }
+`;
+
+const Sidebar = ({ isOpen, setIsOpen }) => {
+  const toggleSide = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <SideBarWrap className={isOpen ? 'open' : ''}>
+      <img
+        className={styles.closeIcon}
+        src="./close.png"
+        alt="close"
+        onClick={toggleSide}
+        onKeyDown={toggleSide}
+      />
+      <ul>
+        <p className={styles.menuitem}>
+          로그인
+        </p>
+        <p className={styles.menuitem}>
+          회원가입
+        </p>
+        <p className={styles.mypagemenu}><Link to="/mypage">
+          마이페이지</Link>
+        </p>
+      </ul>
+    </SideBarWrap>
+  );
+};
+
+export default Sidebar;
