6ed30e23a287dc94da8d4871e0fd5695b3fe8386
simgyuryeol <tlarbfuf@gmail.com>
2023-07-31 10:44:42 +0900

    [FEAT] : Favorite develop
    즐겨찾기 기능 구현 완료
    컨트롤러, 서비스, 리포지토리, 엔티티 구현 완료

---
+++ Ssarout/BackEnd/src/main/java/com/ssafy/ssaout/common/error/ErrorCode.java
@@ -0,0 +1,35 @@
+package com.ssafy.ssaout.common.error;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+
+    INTERNAL_SERVER_ERROR(500, "서버에 문제가 생겼습니다."),
+    INVALID_REFRESH_TOKEN(500, "Invalid refresh token."),
+    INVALID_ACCESS_TOKEN(500, "Invalid access token."),
+    NOT_EXPIRED_TOKEN_YET(500, "Not expired token yet."),
+    OAUTH_PROVIDER_MISMATCH(500, "기존에 가입된 SNS 계정이 있습니다. 해당 계정을 이용해주세요."),
+    FAIL_TOKEN_GENERATION(500, "Failed to generate Token."),
+    CONFLICT_RESOURCE(409,"중복된 값이 있습니다. 다시 입력해주세요");
+
+
+
+    private int status;
+    private String message;
+
+    private static final Map<String, ErrorCode> messageMap = Collections.unmodifiableMap(
+        Stream.of(values()).collect(
+            Collectors.toMap(ErrorCode::getMessage, Function.identity())));
+
+    public static ErrorCode fromMessage(String message) {
+        return messageMap.get(message);
+    }
+}
