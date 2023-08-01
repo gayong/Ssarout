38647fa8cdb272ceab0dc3aaf678658ca3323559
이가영 <lgy4625@naver.com>
2023-08-01 18:01:40 +0900

    #S09P12E203-302 [FEAT] : sns login attach, mypage css

--- front-end/src/pages/MyPage.js
+++ front-end/src/pages/MyPage.js
@@ -1,19 +1,45 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
-import Header from '../components/commonUse/Header'
-// import Footer from '../components/commonUse/Footer';
-import styles from './MyPage.module.css'
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import Header from "../components/commonUse/Header";
+import Footer from "../components/commonUse/Footer";
+import styles from "./MyPage.module.css";
+import Api from "../Api/Api";
 
 const MyPage = () => {
-  const [nickname, setNickname] = useState("Guest")
+  const [nickname, setNickname] = useState("Guest");
+  const [profileUrl, setProfileUrl] = useState("");
+
+  useEffect(() => {
+    f1();
+  }, []);
+
+  const f1 = async () => {
+    try {
+      const response = await Api.get("/api/v1/users");
+      console.log(response.data);
+      setNickname(response.data.data.nickname);
+      setProfileUrl(response.data.data.profileImageUrl);
+    } catch (error) {
+      alert.error(error);
+    }
+  };
 
   return (
     <div>
-      <Header/>
-        <h1>My Page</h1>
-        <h2>{ nickname }님의 마이페이지입니다</h2>
-        <Link to="/update" className={ styles.updateBtn }>회원 정보 수정</Link>
-        <Link to="/singingAI" className= { styles.singingAI }>AI가 불러주는 노래 들으러 가기</Link>
+      <Header />
+      <br/>
+      <div className={styles.profile}>
+        <img className={styles.profileImg} alt="profileImg" src={profileUrl} />
+        <div className={styles.nicknameAndUpdate}>
+          <h2 className={styles.nickname}>{nickname}</h2>
+          <Link to="/update" className={styles.updateBtn}>회원 정보 수정</Link>
+        </div>
+      </div>
+      <br/><br/>
+      <Link to="/singingAI" className={styles.singingAI}>
+        AI가 불러주는 노래 들으러 가기
+      </Link>
+      <Footer />
     </div>
   );
 };
