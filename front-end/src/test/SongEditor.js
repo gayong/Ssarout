5a5113b43b5d05696d3ef2e2c028301f785f37a6
이가영 <lgy4625@naver.com>
2023-08-08 11:03:40 +0900

    [FEAT] : insert record page into localhost

---
+++ front-end/src/test/SongEditor.js
@@ -0,0 +1,92 @@
+import createElem from "./DOMUtil";
+import EventEmitter from "./EventEmitter";
+
+function cBtn(text) {
+  return createElem("button", {}, text);
+}
+
+class SongEditor extends EventEmitter {
+  constructor() {
+    super();
+    this.btnPlay = cBtn("Play");
+    this.btnStop = cBtn("Stop");
+    this.btnSave = cBtn("Save");
+    this.btnUpload = cBtn("Upload");
+    this.btnKeyUp = cBtn("Key Up");
+    this.btnKeyDown = cBtn("Key Down");
+
+    this.inKey = createElem("input", { type: "number", value: "0" });
+
+    this.chkMelody = createElem("input", { type: "checkbox", checked: true });
+    const chkLabel = createElem("label", {}, "play melody");
+    chkLabel.appendChild(this.chkMelody);
+
+    this.inVolume = createElem("input", {
+      type: "range",
+      min: 0,
+      max: 100,
+      value: 30,
+      step: 1,
+    });
+
+    this.inScore = createElem("textarea", { class: "inScore" });
+
+    this.element = createElem("div", { class: "song-editor" }, [
+      chkLabel,
+      this.inVolume,
+      this.btnKeyDown,
+      this.btnKeyUp,
+      this.inScore,
+      this.btnPlay,
+      this.btnStop,
+      // this.btnSave,
+      // this.btnUpload,
+    ]);
+
+    this.btnPlay.addEventListener("click", (e) => {
+      this._clickHandler("play");
+    });
+    this.btnStop.addEventListener("click", (e) => {
+      this._clickHandler("stop");
+    });
+    this.btnKeyDown.addEventListener("click", (e) => {
+      this._clickHandler("key-down");
+    });
+    this.btnKeyUp.addEventListener("click", (e) => {
+      this._clickHandler("key-up");
+    });
+
+    this.chkMelody.addEventListener("input", (e) => {
+      this.emit("change", "melody", this.chkMelody.checked);
+    });
+    this.inVolume.addEventListener("input", (e) => {
+      this.emit("change", "volume", parseInt(this.inVolume.value, 10) / 100);
+    });
+  }
+
+  get key() {
+    return parseInt(this.inKey.value, 10);
+  }
+
+  set key(v) {
+    this.inKey.value = v.toString();
+  }
+
+  _clickHandler(type) {
+    this.emit(type);
+  }
+
+  render() {
+    return this.element;
+  }
+
+  get score() {
+    return this.inScore.value;
+  }
+
+  set score(v) {
+    this.inScore.value = v;
+  }
+}
+
+export default SongEditor;
