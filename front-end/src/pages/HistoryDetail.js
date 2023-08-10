103cfac3f3cbf34cfb6cc32749d1075f0bfaf822
이가영 <lgy4625@naver.com>
2023-08-10 18:28:37 +0900

    [FIX] : modify some css elements

--- front-end/src/pages/HistoryDetail.js
+++ front-end/src/pages/HistoryDetail.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 import Header from "../components/commonUse/Header";
 import Footer from "../components/commonUse/Footer";
 import HistoryDetailCom from "../components/history/HistoryDetailCom"
+import styles from "./HistoryDetail.module.css";
 
 const History = () => {
   return (
     <div>
       <Header/>
+      <button className={styles.updateBackBtn}><Link to="/history" className={styles.updateBackA}>뒤로가기</Link></button>
       <HistoryDetailCom/>
       <Footer/>
     </div>
