f631ddda8fe1f6aff1eb01a0bc75c05a1820310e
swcl <swcl309@naver.com>
2023-08-11 11:40:37 +0900

    [FEAT] : analysisPage & resultPage to api

--- front-end/src/pages/Login.js
+++ front-end/src/pages/Login.js
@@ -6,22 +6,22 @@ import styles from "./Login.module.css"
 const Login = () => {
   const GoogleAuthUrl =
     // 이거는 머지할때마다 바꿔줘야함
-    // "https://i9e203.p.ssafy.io/oauth2/authorization/google?redirect_uri=https://i9e203.p.ssafy.io/oauth/redirect";
+    "https://i9e203.p.ssafy.io/oauth2/authorization/google?redirect_uri=https://i9e203.p.ssafy.io/oauth/redirect";
 
     // 가영이 집에서 할 때
     // "http://i9e203.p.ssafy.io:9090/oauth2/authorization/google?redirect_uri=여기/oauth/redirect";    
    
     // 싸피에서 테스트할 때
-    "http://192.168.30.124:8080/oauth2/authorization/google?redirect_uri=http://localhost:3000/oauth/redirect";
+    // "http://192.168.30.124:8080/oauth2/authorization/google?redirect_uri=http://localhost:3000/oauth/redirect";
   const KakaoAuthUrl =
     // 이거는 머지할때마다 바꿔줘야함
-    // "https://i9e203.p.ssafy.io/oauth2/authorization/kakao?redirect_uri=https://i9e203.p.ssafy.io/oauth/redirect";
+    "https://i9e203.p.ssafy.io/oauth2/authorization/kakao?redirect_uri=https://i9e203.p.ssafy.io/oauth/redirect";
 
     // 가영이 집에서 할 때
     // "http://i9e203.p.ssafy.io:9090/oauth2/authorization/kakao?redirect_uri=여기/oauth/redirect";    
 
     // 싸피에서 테스트할 때
-    "http://192.168.30.124:8080/oauth2/authorization/kakao?redirect_uri=http://localhost:3000/oauth/redirect";
+    // "http://192.168.30.124:8080/oauth2/authorization/kakao?redirect_uri=http://localhost:3000/oauth/redirect";
   const handleGoogleLogin = () => {
     window.location.href = GoogleAuthUrl;
   };
