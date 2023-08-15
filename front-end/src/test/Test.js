d757a253b87535240f56ab0c476b2384915470e6
swcl309 <swcl309@naver.com>
2023-08-16 00:52:34 +0900

    [FIX] line lyrics error

--- front-end/src/test/Test.js
+++ front-end/src/test/Test.js
@@ -210,7 +210,7 @@ export class Test {
       if(data.PitchScore === NaN){
         return
       }
-      console.log(this.BlobUrl)
+      data.lyricsTime = this.songLineList
       data.songTitle = this.songTitle
       data.singer = this.singer
       // if(this.songEditor.score.length > 0){
