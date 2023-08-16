a7f3e491b2809a922449258985de3c83a9dae5bb
hsj4436 <hsj4436@gmail.com>
2023-08-16 09:28:53 +0900

    [REFACTOR] : modify AiCoverResponseDto
    mr file url 같이 주도록 수정

--- Ssarout/BackEnd/src/main/java/com/ssafy/ssaout/aicover/dto/response/AiCoverResponseDto.java
+++ Ssarout/BackEnd/src/main/java/com/ssafy/ssaout/aicover/dto/response/AiCoverResponseDto.java
@@ -11,6 +11,7 @@ public class AiCoverResponseDto {
     private final String title;
     private final String singer;
     private final String albumCoverImage;
+    private final String mrFile;
     private final String aiCoverFile;
     private final LocalDateTime createdDateTime;
 
@@ -19,6 +20,7 @@ public class AiCoverResponseDto {
         this.title = aiCover.getSong().getTitle();
         this.singer = aiCover.getSong().getSinger();
         this.albumCoverImage = aiCover.getSong().getAlbumCoverImage();
+        this.mrFile = aiCover.getSong().getMrFile();
         this.aiCoverFile = aiCover.getAiCoverFile();
         this.createdDateTime = aiCover.getCreatedDateTime();
     }
