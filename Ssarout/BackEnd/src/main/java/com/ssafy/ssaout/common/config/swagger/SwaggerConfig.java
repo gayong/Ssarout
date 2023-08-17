7bdea5d071ee3b76fdf2b2adb311d316d47ee47f
hsj4436 <hsj4436@gmail.com>
2023-08-17 16:12:41 +0900

    [FEAT] : add Swagger

---
+++ Ssarout/BackEnd/src/main/java/com/ssafy/ssaout/common/config/swagger/SwaggerConfig.java
@@ -0,0 +1,43 @@
+package com.ssafy.ssaout.common.config.swagger;
+
+import java.util.Arrays;
+import java.util.List;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.OAS_30).securityContexts(
+                Arrays.asList(securityContext()))
+            .securitySchemes(Arrays.asList(apiKey())).select().apis(RequestHandlerSelectors.any())
+            .paths(PathSelectors.any()).build();
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder().securityReferences(defaultAuth()).build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global",
+            "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Arrays.asList(new SecurityReference("Authorization", authorizationScopes));
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("Authorization", "JWT", "header");
+    }
+
+}
--- Ssarout/BackEnd/src/main/java/com/ssafy/ssaout/fav/controller/FavoriteController.java
+++ Ssarout/BackEnd/src/main/java/com/ssafy/ssaout/fav/controller/FavoriteController.java
@@ -4,6 +4,9 @@ import com.ssafy.ssaout.common.response.ApiResponse;
 import com.ssafy.ssaout.fav.dto.FavDto;
 import com.ssafy.ssaout.fav.dto.FavSongDto;
 import com.ssafy.ssaout.fav.service.FavoriteService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -13,6 +16,7 @@ import java.util.List;
 
 import static org.springframework.http.HttpStatus.OK;
 
+@Api(tags = "즐겨찾기 APIs")
 @RestController
 @RequestMapping("/api/v1/fav")
 @RequiredArgsConstructor
@@ -24,8 +28,9 @@ public class FavoriteController {
      * 등록 되어 있는 경우 삭제
      * 신규일 경우 등록
      */
+    @ApiOperation(value = "즐겨찾기 등록, 삭제", notes = "즐겨찾기가 등록되어 있는 경우 삭제, 그렇지 않을 경우 등록합니다.")
     @PostMapping
-    public ResponseEntity fav_save(@RequestBody FavDto favDto){
+    public ResponseEntity fav_save(@ApiParam(value = "즐겨찾기할 곡 ID") @RequestBody FavDto favDto){
 
         org.springframework.security.core.userdetails.User principal = (org.springframework.security.core.userdetails.User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         String userId = principal.getUsername();
@@ -44,6 +49,7 @@ public class FavoriteController {
     /**
      * 즐겨 찾기 목록
      */
+    @ApiOperation(value = "즐겨찾기 목록 조회", notes = "사용자가 등록한 즐겨찾기 목록을 조회합니다.")
     @GetMapping
     public ResponseEntity<ApiResponse> fav_list(){
         org.springframework.security.core.userdetails.User principal = (org.springframework.security.core.userdetails.User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
